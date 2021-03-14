4f27eb9cff5cef1ecd2b869c6534f53523c8079a
sarismet <sarismet2825@gmail.com>
2021-03-14 19:58:24 +0300

    creating paper is added

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -25,10 +26,20 @@ type Paper struct {
 
 var databaseUrl string = ""
 
+func CreatePaper(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var paper Paper
+	_ = json.NewDecoder(request.Body).Decode(&paper)
+	collection := client.Database("Blog").Collection("papers")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, paper)
+	json.NewEncoder(response).Encode(result)
+}
+
 func main() {
 	fmt.Println("Hello world")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(databaseUrl))
 	if err != nil {
 		log.Fatal(err)
@@ -38,7 +49,7 @@ func main() {
 		fmt.Println("Hello Database")
 	}
 	router := mux.NewRouter()
-
+	router.HandleFunc("/papers/add", CreatePaper).Methods("POST")
 	http.ListenAndServe(":8000", router)
 
 }
